217b30e5f766abdec3a963e9d3cb5cf424e3d838
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:28:19 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan3-input/src/pbo1/pkg10118002/latihan3/input/PBO110118002Latihan3Input.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan3.input;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ */
+public class PBO110118002Latihan3Input {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        System.out.print("Masukkan nama anda: ");
+    Scanner scanner = new Scanner(System.in);
+    
+    String nama = scanner.next();
+        System.out.println("Nama anda adalah "+nama);
+    }
+    
+}
